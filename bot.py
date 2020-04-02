a717f3087c7c0a8bee0ff23c2466f2b9fc998136
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2020-04-02 01:02:03 -0400

    fix commands with on_msg

--- bot.py
+++ bot.py
@@ -15,12 +15,20 @@ async def on_ready():
     print("Smite bot is now running...")
 
 
+@client.command()
+async def test(context):
+    await context.send("test reply")
+
+
 @client.event
 async def on_message(message):
     log_messages(message)
 
     if message.author == client.user:
         return
+
+    # this line is included in the default on_message coroutine
+    await client.process_commands(message)
 
 
 def main():
